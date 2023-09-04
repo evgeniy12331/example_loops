6590adfdf1cb10c31407447f57fd5de91364aa43
Evgeniy Sorokin <evgeniy.sorokin955@gmail.com>
2023-09-04 18:53:10 +0300

    изменненый текст

---
+++ learn_loop_while/lesson8.py
@@ -0,0 +1,11 @@
+n = 100 // 2
+while True:
+    print('число равно , больше или меньше ',n)
+    answer = int(input('1 - равно , 2 - больше , 3 - меньше '))
+    if answer == 1:
+        print('угадали')
+        break
+    elif answer == 2:
+        n +=1
+    else:
+        n -=1
